7bf1f8c75367bbffed9ec1cac4682ed6bec363ba
lbaf23 <56927357+lbaf23@users.noreply.github.com>
2022-04-09 20:54:11 +0800

    add requests file

--- attack.py
+++ attack.py
@@ -1,4 +1,7 @@
+import shutil
 import zipfile
+
+import requests
 import sa
 import os
 
@@ -12,19 +15,32 @@ def attack(task_id, file_url):
 
 def unzip_file(name):
     zip_file = zipfile.ZipFile(name)
-    zip_file.extractall('./')
+    zip_file.extractall('user_model')
     zip_file.close()
     os.rename(zip_file.filename.split('.')[0], "user_model")
     return 'user_model'
 
 
 def get_file(url):
+    del_all()
     if(url.startswith('http')):
-        # download file to current dir
-        pass
+        r = requests.get(url=url, stream=True)
+        f = open('user_model.zip', 'wb')
+        for chunk in r.iter_content(chunk_size=1024):
+            if chunk:
+                f.write(chunk)
+        f.close()
+        return 'user_model.zip'
     else:
         return url
 
 
-p = unzip_file(get_file('user_model00.zip'))
+def del_all():
+    if(os.path.exists('user_model')):
+        shutil.rmtree('user_model')
+    if(os.path.exists('user_model.zip')):
+        os.remove('user_model.zip')
+
+
+p = unzip_file(get_file('http://localhost:8000/files/ptm-leaderboard/user1/sa/user_model00.zip?t=1649491872947076400'))
 print(p)
