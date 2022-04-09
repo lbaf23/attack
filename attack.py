e07199faf955c8429e31237dd88c5501a232a483
lbaf23 <2282480289@qq.com>
2022-04-09 11:00:30 +0800

    update

---
+++ attack.py
@@ -0,0 +1,31 @@
+import zipfile
+import sa
+import os
+
+def attack(task_id, file_url):
+    model_path = unzip_file(get_file(file_url))
+    res = {}
+    if(task_id == 'sa'):
+        res = sa.sa_attack(model_path)
+    return res
+
+
+def unzip_file(name):
+    zip_file = zipfile.ZipFile(name)
+    zip_file.extractall('./')
+    zip_file.close()
+    os.rename(zip_file.filename.split('.')[0], "user_model")
+    return 'user_model'
+
+
+
+def get_file(url):
+    if(url.startswith('http')):
+        # download file to current dir
+        pass
+    else:
+        return url
+
+
+p = unzip_file(get_file('user_model1234567890.zip'))
+print(p)
---
+++ sa/sa_attack.py
@@ -0,0 +1,32 @@
+import OpenAttack as oa
+import datasets
+import transformers
+
+def dataset_mapping(x):
+    return {
+        "x": x["sentence"],
+        "y": 1 if x["label"] > 0.5 else 0,
+    }
+    
+# attack a model
+def sa_attack(model_path):
+
+    # load some examples of SST-2 for evaluation
+    dataset = datasets.load_dataset("sst", split="train[:5]").map(function=dataset_mapping)
+    # choose the costomized classifier as the victim model
+
+    
+    tokenizer = transformers.AutoTokenizer.from_pretrained(model_path)
+    model = transformers.AutoModelForSequenceClassification.from_pretrained(model_path, num_labels=2, output_hidden_states=False)
+
+
+    victim = oa.classifiers.TransformersClassifier(model, tokenizer, model.bert.embeddings.word_embeddings)
+
+
+    # choose PWWS as the attacker and initialize it with default parameters
+    attacker = oa.attackers.PWWSAttacker()
+    # prepare for attacking
+    attack_eval = oa.AttackEval(attacker, victim)
+    # launch attacks and print attack results 
+    res = attack_eval.eval(dataset, visualize=False)
+    return res
