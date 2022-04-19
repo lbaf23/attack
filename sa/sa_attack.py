7d45efbbf77a28460a4acb88556287c2c9ab097c
lbaf23 <56927357+lbaf23@users.noreply.github.com>
2022-04-19 10:23:10 +0800

    Update sa_attack.py

--- sa/sa_attack.py
+++ sa/sa_attack.py
@@ -13,13 +13,11 @@ def dataset_mapping(x):
 def sa_attack(model_path):
     dataset = datasets.load_dataset("sst", split="train[:10]").map(function=dataset_mapping)
     tokenizer = transformers.AutoTokenizer.from_pretrained(model_path)
-
-    # model = transformers.AutoModelForSequenceClassification.from_pretrained(model_path, num_labels=2, output_hidden_states=False)
-    model = transformers.AutoModelForSequenceClassification.from_pretrained(model_path)
+    model = transformers.AutoModelForSequenceClassification.from_pretrained(model_path, num_labels=2, output_hidden_states=False)
     victim = oa.classifiers.TransformersClassifier(model, tokenizer, model.bert.embeddings.word_embeddings)
     attacker = oa.attackers.PWWSAttacker()
     attack_eval = oa.AttackEval(attacker, victim)
-    res = attack_eval.eval(dataset, visualize=True)
+    res = attack_eval.eval(dataset, visualize=False, progress_bar=True)
     return res
 
 
