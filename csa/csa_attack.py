e752d5508023936930b38449a38da555bf3bd6f4
lbaf23 <56927357+lbaf23@users.noreply.github.com>
2022-04-19 16:59:33 +0800

    Update csa_attack.py

--- csa/csa_attack.py
+++ csa/csa_attack.py
@@ -9,25 +9,37 @@ def csa_attack(model_path):
     model = transformers.AutoModelForSequenceClassification.from_pretrained(model_path, num_labels=2, output_hidden_states=False)
     victim = oa.classifiers.TransformersClassifier(model, tokenizer, model.bert.embeddings.word_embeddings)
 
+    success = 0
+    total = 0
+
     attacker = oa.attackers.PWWSAttacker(lang="chinese")
     attack_eval = oa.AttackEval(attacker, victim)
-    res = attack_eval.eval(dataset, visualize=False)
+    res = attack_eval.eval(dataset, visualize=False, progress_bar=True)
     print(res)
+    success = success + res.get("Successful Instances")
+    total = total + res.get("Total Attacked Instances")
+
 
     attacker = oa.attackers.GeneticAttacker(lang="chinese")
     attack_eval = oa.AttackEval(attacker, victim)
-    res = attack_eval.eval(dataset, visualize=False)
+    res = attack_eval.eval(dataset, visualize=False, progress_bar=True)
     print(res)
+    success = success + res.get("Successful Instances")
+    total = total + res.get("Total Attacked Instances")
+
 
     attacker = oa.attackers.TextFoolerAttacker(lang="chinese")
     attack_eval = oa.AttackEval(attacker, victim)
-    res = attack_eval.eval(dataset, visualize=False)
+    res = attack_eval.eval(dataset, visualize=False, progress_bar=True)
     print(res)
+    success = success + res.get("Successful Instances")
+    total = total + res.get("Total Attacked Instances")
 
-    return res
+    score = (total - success) * 100.0 / total
+    return score
 
 
 model_path = 'hfl/chinese-bert-wwm'
 
-
-csa_attack(model_path)
+res = csa_attack(model_path)
+print(res)
