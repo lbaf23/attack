d2d16ceba81a6a099e7122df3d080f2ec5dff8d9
lbaf23 <56927357+lbaf23@users.noreply.github.com>
2022-04-19 10:28:51 +0800

    Update sa_attack.py

--- sa/sa_attack.py
+++ sa/sa_attack.py
@@ -22,10 +22,7 @@ def sa_attack(model_path):
 
 
 # model_path = 'echarlaix/bert-base-uncased-sst2-acc91.1-d37-hybrid'
-# model_path = 'mrm8488/bert-mini-finetuned-age_news-classification'
-# model_path = 'xaqren/sentiment_analysis'
 # model_path = 'lannelin/bert-imdb-1hidden'
-# model_path = 'gchhablani/bert-base-cased-finetuned-mnli'
 model_path = "rohanrajpal/bert-base-multilingual-codemixed-cased-sentiment"
 
 sa_attack(model_path)
